a94b3c64561aa9ce36eeb2451193d03e08fb8a12
kdragonkorea <morenmore6340@gmail.com>
2021-02-09 09:05:31 +0900

    안심병원 데이터 구축

--- projectapp/models.py
+++ projectapp/models.py
@@ -1,13 +1,10 @@
 from django.db import models
 
 class Hospital(models.Model) :
-    id = models.IntegerField(primary_key=True, serialize=False)
-    region = models.CharField(max_length=50)
-    name = models.CharField(max_length=50)
+    name = models.CharField(primary_key=True, max_length=50)
     address = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
     number = models.IntegerField(null=True)
 
     def __str__(self):
-        return self.id+","+str(self.region)+","+str(self.h_name)+","+str(self.address)+\
-               ","+str(self.h_type) + ","+(self.number)
+        return self.name + "," + (self.address) + "," + (self.type) + ","+str(self.number)
