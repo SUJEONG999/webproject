18e33084f6f56fb30848a5dd03e22ea36ed916e1
kdragonkorea <morenmore6340@gmail.com>
2021-02-10 10:49:32 +0900

    add class Clinic(models.Model)

--- projectapp/models.py
+++ projectapp/models.py
@@ -8,3 +8,11 @@ class Hospital(models.Model) :
 
     def __str__(self):
         return self.name + "," + (self.address) + "," + (self.type) + ","+str(self.number)
+
+class Clinic(models.Model) :
+    name = models.CharField(primary_key=True, max_length=50)
+    address = models.CharField(max_length=50)
+    number = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name + "," + (self.address) + "," + str(self.number)
