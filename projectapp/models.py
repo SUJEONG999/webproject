e160cad4fffd96bc9a10f4228c61f50951649211
kdragonkorea <morenmore6340@gmail.com>
2021-02-16 01:08:45 +0900

    update team members files

--- projectapp/models.py
+++ projectapp/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from uuid import uuid4
+from datetime import datetime
+from django.contrib.auth.models import User
 
 class Hospital(models.Model) :
     h_id = models.IntegerField(primary_key=True)
@@ -21,3 +24,25 @@ class Restaurant(models.Model) :
     r_gu = models.CharField(max_length=10)
     r_name = models.TextField()
     r_address = models.TextField()
+
+# 하영
+class Board(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, null=True, verbose_name='작성자')
+    postname = models.CharField(max_length=50, verbose_name='제목')
+    contents = models.TextField(verbose_name='내용')
+    mainphoto = models.ImageField(blank=True, null=True, verbose_name='사진')
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+#관리자 페이지에서 정상적으로 title 테이블 출력
+    def __str__(self):
+        return self.title
+
+
+@property
+def click(self):
+    self.hits += 1
+    self.save()
+
+def board():
+    return None
--- projectapp/urls.py
+++ projectapp/urls.py
@@ -6,7 +6,12 @@ urlpatterns = [
     path('map1/', views.map1, name = 'map1'),
     path('map2/', views.map2, name = 'map2'),
     path('map2_1/', views.map2_1, name='map2_1'),
-    path('register/', views.register, name='register'),  # 수정's 추가
-    path('login/', views.login, name='login'),  # 수정's 추가
-    path('logout/', views.logout, name='logout'),  # 수정's 추가
+    path('board/', views.board, name = 'board'),
+    path('register/', views.register, name= 'register'), # 수정's 추가
+    path('login/', views.login, name= 'login'), # 수정's 추가
+    path('logout/', views.logout, name= 'logout'), # 수정's 추가
+    path('board/<int:pk>/', views.board_view, name='board_view'),
+    path('board_write/', views.board_write, name='board_write'),
+    path('write/', views.write, name='write'),
+    path('board/<int:pk>/remove/', views.remove_board),
 ]
