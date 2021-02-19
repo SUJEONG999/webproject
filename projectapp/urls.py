79a29ce9b9149b2917ec30f7006462183df6f45f
Yoon-Sangwon <tkddnjs1648@gmail.com>
2021-02-19 23:13:31 +0900

    Delete All because of changing template

--- projectapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('main/', views.main, name = 'main'),
-    path('map1/', views.map1, name = 'map1'),
-    path('map2/', views.map2, name = 'map2'),
-    path('board/', views.board, name = 'board'),
-]
--- testapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def test(request):
-    return  HttpResponse("<h1>Test 완료!!!</h1>")
-# Create your views here.
